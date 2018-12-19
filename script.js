88ff04f7e2b64f5aad3d2fee51fdbe3951d50541
Krez90 <bilel.hamimed@yahoo.fr>
2018-12-19 20:45:35 +0100

    maj_mercredi

---
+++ script.js
@@ -0,0 +1,7 @@
+// var perenoyel = document.getElementById('pere_x5F_noyel');
+// var switcher = 1;
+
+// perenoyel.addEventListener('animationiteration', function(){
+//     switcher = switcher * -1;
+//     perenoyel.style.transform= "scaleX("+switcher+")";
+// });
